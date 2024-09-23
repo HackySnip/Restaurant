e12b663d3ceb5f30ea63c8346f977024a2d079a4
RushabhBhosale <bhosalerushabh35@gmail.com>
2024-09-23 22:47:07 +0530

    restaurant controller added

---
+++ server/controller/restaurant.controller.ts
@@ -0,0 +1,206 @@
+import { Request, Response } from "express";
+import { Restaurant } from "../models/restaurant.model";
+import { Multer } from "multer";
+import uploadImageOnCloudinary from "../utils/imageUpload";
+import { json } from "stream/consumers";
+import { Order } from "../models/order.model";
+
+export const createRestaurant = async (req: Request, res: Response) => {
+  try {
+    const { restaurantName, city, country, price, deliveryTime, cuisines } =
+      req.body;
+
+    const file = req.file;
+
+    const restaurant = await Restaurant.findOne({ user: req.id });
+
+    if (restaurant) {
+      return res.status(400).json({
+        success: false,
+        message: "Restaurant already exists for this user",
+      });
+    }
+
+    if (!file) {
+      return res.status(400).json({
+        success: false,
+        message: "Restaurant image is required",
+      });
+    }
+
+    const imageUrl = await uploadImageOnCloudinary(file as Express.Multer.File);
+
+    await Restaurant.create({
+      user: req.id,
+      restaurantName,
+      city,
+      country,
+      deliveryTime,
+      cuisines: JSON.parse(cuisines),
+      imageUrl,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Restaurant created successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.id });
+
+    if (!restaurant) {
+      return res.status(400).json({
+        success: false,
+        message: "Restaurant not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      data: { restaurant },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const updateRestaurant = async (req: Request, res: Response) => {
+  try {
+    const { restaurantName, city, country, deliveryTime, cuisines } = req.body;
+    const file = req.file;
+    const restaurant = await Restaurant.findOne({ user: req.id });
+    if (!restaurant) {
+      return res.status(400).json({
+        success: false,
+        message: "Restaurant not found",
+      });
+    }
+
+    restaurant.restaurantName = restaurantName;
+    restaurant.city = city;
+    restaurant.country = country;
+    restaurant.deliveryTime = deliveryTime;
+    restaurant.cuisines = JSON.parse(cuisines);
+    if (file) {
+      const imageUrl = await uploadImageOnCloudinary(
+        file as Express.Multer.File
+      );
+      restaurant.imageUrl = imageUrl;
+    }
+    await restaurant.save();
+    return res.status(200).json({
+      success: true,
+      message: "Restaurant updated successfully",
+      data: { restaurant },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const restaurantOrders = async (req: Request, res: Response) => {
+  try {
+    const restaurant = await Restaurant.findOne({ user: req.id });
+    if (!restaurant) {
+      return res.status(400).json({
+        success: false,
+        message: "Restaurant not found",
+      });
+    }
+
+    const orders = await Order.find({ restaurant: restaurant._id })
+      .populate("restaurant")
+      .populate("user");
+
+    return res.status(200).json({
+      success: true,
+      data: { orders },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const updateOrderStatus = async (req: Request, res: Response) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(400).json({
+        success: false,
+        message: "Order not found",
+      });
+    }
+    order.status = status;
+    await order.save();
+    return res.status(200).json({
+      success: true,
+      message: "Order status updated",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const searchRestaurant = async (req: Request, res: Response) => {
+  try {
+    const searchText = req.params.searchText || "";
+    const searchQuery = (req.query.searchQuery as string) || "";
+    const selectedCuisines = ((req.query.selectedCuisines as string) || "")
+      .split(",")
+      .filter((cuisine) => cuisine);
+
+    const query: any = {};
+    if (searchText) {
+      query.$or = [
+        { restuarantName: { $regex: searchText, $options: "i" } },
+        { city: { $regex: searchText, $options: "i" } },
+        { country: { $regex: searchText, $options: "i" } },
+      ];
+    }
+
+    if (searchQuery) {
+      query.$or = [
+        { restuarantName: { $regex: searchText, $options: "i" } },
+        { cuisines: { $regex: searchQuery, $options: "i" } },
+      ];
+    }
+
+    if (selectedCuisines.length > 0) {
+      query.cuisines = { $in: selectedCuisines };
+    }
+
+    const restaurants = await Restaurant.find(query);
+
+    return res.status(200).json({
+      success: true,
+      data: { restaurants },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
